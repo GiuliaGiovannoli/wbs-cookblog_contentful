debc764a598d12cde59517d6e6177b0e3b8e9f66
Giulia Giovannoli <giovannoli.giulia@gmail.com>
2021-02-23 17:55:32 +0100

    still deploying

--- my-app/src/client.js
+++ my-app/src/client.js
@@ -2,6 +2,6 @@ import * as contentful from 'contentful';
 
 export const client = 
 contentful.createClient({
-    space: process.env.development.REACT_APP_SPACE_ID,
-    accessToken: process.env.development.REACT_APP_ACCESS_TOKEN
+    space: process.env.REACT_APP_SPACE_ID,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 })
